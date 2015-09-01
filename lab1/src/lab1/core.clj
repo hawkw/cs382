3d99b9719a4c17e0c6461df676b50742a6047102
Hawk Weisman <hi@hawkweisman.me>
2015-09-01 16:00:14 -0400

    recreate lab 1 after I nuked it (oops)

---
+++ lab1/src/lab1/core.clj
@@ -0,0 +1,17 @@
+(ns lab1.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [lab1.shader :as shader]))
+
+
+(q/defsketch lab1
+  :title "Space!"
+  :size [500 500]
+  ; setup function called only once, during sketch initialization.
+  :setup shader/setup
+  ; update-state is called on each iteration before draw-state.
+  :update shader/update-state
+  :draw shader/draw-state
+  :features [:keep-on-top]
+  ; This sketch uses functional-mode middleware.
+  :middleware [m/fun-mode])
---
+++ lab1/src/lab1/shader.clj
@@ -0,0 +1,11 @@
+
+(defn setup []
+  ; Set frame rate to 360 frames per second.
+  (q/frame-rate 60)
+  )
+
+(defn update-state [state])
+
+(defn draw-state [state]
+  ; reset background to black
+  (q/background 0))
