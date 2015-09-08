46ba2ebe324de68b18f411671b845c38a355b816
Hawk Weisman <hi@hawkweisman.me>
2015-09-08 12:18:35 -0400

    Add README & comments

--- lab1/README.md
+++ lab1/README.md
@@ -1,6 +1,8 @@
-# lab1
+# CMPSC382 Lab 1 - Hawk Weisman
 
-A Quil sketch designed to ... well, that part is up to you.
+For the first CMPSC382 lab assignment, I created an animation that 'simulates' flying through space, like the `starfield` screen saver that ships with Windows. The stars are simulated as though they exist in a three-dimensional space, with an x-, y-, and z-dimension, and projected onto the 2D screen. Stars that are 'farther away' from the viewer in the z-dimension are dimmed according to their z-distance. As the program runs, the stars are moved towards the viewer to simulate flying through space, with new stars being generated randomly as old ones leave the screen boundary.
+
+Since I was already familiar with programming in Processing, I chose to use [Quil](http://quil.info/), a library for the Clojure programming language that wraps Processing and provides access to its functions. I've been meaning to learn more about Clojure for some time, and this assignment gave me a good opportunity to practice programming in that language.
 
 ## Usage
 
@@ -9,10 +11,3 @@ LightTable - open `core.clj` and press `Ctrl+Shift+Enter` to evaluate the file.
 Emacs - run cider, open `core.clj` and press `C-c C-k` to evaluate the file.
 
 REPL - run `(require 'lab1.core)`.
-
-## License
-
-Copyright © 2014 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
--- lab1/src/lab1/shader.clj
+++ lab1/src/lab1/shader.clj
@@ -2,6 +2,9 @@
     (:require [quil.core :as q]))
 
 (defn create-star []
+    ; create a new star at the selected x-, y-, and z-position.
+    ; this function should be called repeatedly when creating the initialization
+    ; state, or whenever a star goes out of bounds.
     {:x (q/random 0 1000)
      :y (q/random 0 1000)
      :z (q/random 100 1000)})
@@ -12,19 +15,30 @@
   {:stars (repeatedly 64 #(create-star)) })
 
  (defn screen-pos [n z dim]
-    ; let c equal the screen centre in this dimension
-    (let [c (/ 2 dim)]; in
-    ; find the bias to shift the star in this dimension
+    ; determine the screen position for an object in the
+    ; simulated space in one dimension, where `n` is the x-or y-position
+    ; of the object, `z` is the z-position of the object, and `dim` is the
+    ; size of the screen in that dimension
+    (let ; let `c` equal the screen centre in this dimension
+        [c (/ 2 dim)]; in
+        ; find the bias to shift the star in this dimension
          (* (/ n z)
             (+ 100 c) )))
 
 (defn screen-x [x z]
+    ; determine the screen x-position for an object with the given
+    ; x- and z-positions in the simulated space
     (screen-pos x z (q/width)))
 
 (defn screen-y [y z]
+    ; determine the screen y-position for an object with the given
+    ; y- and z-positions in the simulated space
     (screen-pos y z (q/height)))
 
 (defn update-star [star]
+    ; update a given star by decreasing its z-distance from the
+    ; viewer, or by generating a new star if the star has moved
+    ; off the screen
     (let [z (:z star)
           y (screen-y (:y star) z)
           x (screen-x (:x star) z)]; in
@@ -37,8 +51,10 @@
 
 
 (defn update-state [state]
-        (update-in state [:stars]
-            #(map update-star %1)))
+    ; update the entire state by mapping `update-star` across each star.
+    ; this is called once per frame, before drawing the state.
+    (update-in state [:stars]
+        #(map update-star %1)))
 
 (defn grey-from-z [z]
     ; use z-distance to fade the star
@@ -46,14 +62,16 @@
     (- 255 (* z (/ 255 900))))
 
 (defn draw-star [x y z]
+    ; draws a star at position (`x`, `y`, `z`) by setting the pixel
+    ; at the screen x- and y-position corresponding to that star
+    ; with a grey value calculated from the star's distance
     (q/set-pixel (screen-x x z)
                  (screen-y y z)
                  (q/color (grey-from-z z))))
 
 (defn draw-state [state]
-    ; reset background to black
-    (q/background 0)
-    ; draw stars
-    (doseq [{x :x y :y z :z} (:stars state)]
-        ; (println (str "x: " (screen-x x z) ", y: " (screen-y y z)))
-        (draw-star x y z)))
+    ; draws a state
+    (q/background 0) ; reset background to black (if this is commented out,
+                     ; we can get some kind of nift streaks...)
+    (doseq [{x :x y :y z :z} (:stars state)] ; for each star in the state...
+        (draw-star x y z))) ; ...call the `draw-star `function
