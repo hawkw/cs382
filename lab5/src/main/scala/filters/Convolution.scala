25fefc8ceb7c5cddd2d2de88ade90c2ffb6a1a08
Hawk Weisman <hi@hawkweisman.me>
2015-10-09 16:31:53 -0400

    More typedefs

--- lab5/src/main/scala/filters/Convolution.scala
+++ lab5/src/main/scala/filters/Convolution.scala
@@ -6,5 +6,9 @@ trait Convolution {
 
   type Pixel = Int
   type Image = Array[Array[Pixel]]
-  
+  type Weights = Array[Array[Float]]
+
+  def convolution(w: Weights)(i: Image): Image
+    = ???
+
 }
