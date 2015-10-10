bef98b551d3362dc45dd0cfb63be5552a087e8cb
Hawk Weisman <hi@hawkweisman.me>
2015-10-10 13:31:29 -0400

    Suddenly you can do a lot more stuff to colors

--- lab5/src/main/scala/filters/FunColors.scala
+++ lab5/src/main/scala/filters/FunColors.scala
@@ -5,7 +5,18 @@ import PConstants._
 import PApplet._
 
 /**
- * Mixin trait for `PApplet`s that provides a better functional color API.
+ * Mixin trait for `PApplet`s that provides a Scala idiomatic color API.
+ *
+ * The [[FunColors]] API is both more functional and more fun to use,
+ * thus the name. :)
+ *
+ * Unfortunately, it is necessary to set the color mode quite frequently within
+ * the [[FunColors]] API, which may cause some minor performance issues. This
+ * is because we cannot guarantee that Processing is in the correct color mode.
+ * It might be possible to figure out a more efficient way to do this if we can
+ * find a way to guarantee that Processing is in a particular color mode
+ * prior to using this API.
+ *
  * @author Hawk Weisman
  */
 trait FunColors
@@ -14,19 +25,71 @@ extends PApplet {
   implicit class Color(val c: Int) {
     def red: Int
       = red c
-    def blue: Int
-      = blue c
+
     def green: Int
       = green c
 
+    def blue: Int
+      = blue c
+
     def red_=(r: Int): Color
-      = color(r, blue, green)
+      = { colorMode(RGB)
+          color(r, blue, green)
+        }
 
     def green_=(g: Int): Color
-      = color(red, blue, g)
+      = { colorMode(RGB)
+          color(red, g, blue)
+        }
 
     def blue_=(b: Int): Color
-      = color(red, b, green)
+      = { colorMode(RGB)
+          color(red, green, b)
+        }
+
+    def red_+=(r: Int): Color
+      = { colorMode(RGB)
+          color(red + r, blue, green)
+        }
+
+    def green_+=(g: Int): Color
+      = { colorMode(RGB)
+          color(red, green + g, blue)
+        }
+
+    def blue_+=(b: Int): Color
+      = { colorMode(RGB)
+          color(red, green, blue + b)
+        }
+
+    def red_-=(r: Int): Color
+      = { colorMode(RGB)
+          color(red - r, blue, green)
+        }
+
+    def green_-=(g: Int): Color
+      = { colorMode(RGB)
+          color(red, green - g, blue)
+        }
+
+    def blue_-=(b: Int): Color
+      = { colorMode(RGB)
+          color(red, green, blue - b)
+        }
   }
 
+}
+
+trait FunRGBColors
+extends FunColors {
+
+  object Color {
+    def unapply(c: Color): Option[(Int, Int, Int)]
+      = Some((c.red, c.blue, c.green))
+
+    def apply(red: Int, blue: Int, green: Int): Color
+      = { colorMode(RGB)
+          color(red, green, blue)
+        }
+  }
 }
