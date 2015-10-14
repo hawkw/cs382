d85785d9417f68fd74ddcd1360ce14c395e8a161
Hawk Weisman <hi@hawkweisman.me>
2015-10-14 14:33:19 -0400

    Additional work on convolute

--- lab5/src/main/scala/filters/Convolution.scala
+++ lab5/src/main/scala/filters/Convolution.scala
@@ -1,11 +1,39 @@
-package filters;
+package filters
+
+import processing.core._
+import PApplet._
+
+import scala.language.postfixOps
 
 trait Convolution
-extends ImageProcessing {
+  extends ImageProcessing
+  with FunColors {
 
   type Weights = Array[Array[Float]]
 
-  def convolution(w: Weights)(i: Image): Image
+  implicit class ArrayWithZip2DIndex[A](val xs: Array[Array[A]]) {
+
+    def zipWith2DIndex: Array[Array[(A, Int, Int)]]
+    = for {(row, i) ← xs.zipWithIndex
+           j ← row indices}
+      yield row map {
+        (_, i, j)
+      }
+
+  }
+
+  def convolution(matrix: Weights)(i: Image): Image
+  = {
+    val offset = floor(matrix.length / 2)
+
+    val convolute: (Pixel, Int, Int) ⇒ Pixel
     = ???
+
+    i.zipWith2DIndex
+      .map {
+        _ map (convolute tupled)
+      }
+
+  }
 
 }
